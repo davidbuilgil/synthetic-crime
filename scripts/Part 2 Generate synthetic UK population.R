867c83baafc2117b9e1e78a6eb79e0a875ef964c
David Buil-Gil <david.builgil@manchester.ac.uk>
2020-12-16 14:26:05 +0000

    synthetic-population-data

--- scripts/Part 2 Generate synthetic UK population.R
+++ scripts/Part 2 Generate synthetic UK population.R
@@ -217,5 +217,20 @@ rm(list=c("syn_res_OA", "cor", "cor_d", "cor_d2", "sigma", "syn_res_OA_d", "syn_
 syn_res_OA <- syn_res_OA_D %>%
   dplyr::select(ID, OA11CD, Age, Male, BornUK, White, No_income, Married, High_edu)
 
+#split in seven files to allow upload onto Github
+syn_res_OA.a <- syn_res_OA %>% filter(ID <= 8010845)
+syn_res_OA.b <- syn_res_OA %>% filter(ID >  8010845 & ID <= 16021690)
+syn_res_OA.c <- syn_res_OA %>% filter(ID >  16021690 & ID <= 24032535)
+syn_res_OA.d <- syn_res_OA %>% filter(ID >  24032535 & ID <= 32043380)
+syn_res_OA.e <- syn_res_OA %>% filter(ID >  32043380 & ID <= 40054225)
+syn_res_OA.f <- syn_res_OA %>% filter(ID >  40054225 & ID <= 48065070)
+syn_res_OA.g <- syn_res_OA %>% filter(ID >  48065070 & ID <= 56075912)
+
 #save synthetic UK population as RData
-save(syn_res_OA, file = here("data", "synthetic_population.RData"))
+save(syn_res_OA.a, file = here("data", "synthetic_population_a.RData"))
+save(syn_res_OA.b, file = here("data", "synthetic_population_b.RData"))
+save(syn_res_OA.c, file = here("data", "synthetic_population_c.RData"))
+save(syn_res_OA.d, file = here("data", "synthetic_population_d.RData"))
+save(syn_res_OA.e, file = here("data", "synthetic_population_e.RData"))
+save(syn_res_OA.f, file = here("data", "synthetic_population_f.RData"))
+save(syn_res_OA.g, file = here("data", "synthetic_population_g.RData"))
